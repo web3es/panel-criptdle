1aa012d56dafaf313a13d90dff2a825719a38ec7
mmednik <mmednik@gmail.com>
2022-04-07 10:30:12 -0300

    agrego algunos comentarios al código

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import { ethers } from "ethers";
-import { useState } from "react";
-import Criptdle from "./contract/Cripdle.json";
 
-const criptdleAddress = "0xc13A13144D2E68ACa1e1e6Fd6d75aADD4737814C";
+import { ethers } from "ethers"; // librería para interactuar con nuestro smart contract
+import { useState } from "react";
+
+import Criptdle from "./contract/Cripdle.json"; // ABI
+
+const criptdleAddress = "0xc13A13144D2E68ACa1e1e6Fd6d75aADD4737814C"; // la dirección de nuestro smart contract
 
 function App() {
-
-  const [words, setWords] = useState([]);
-  const [word, setWord] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const [words, setWords] = useState([]); // para guardar las palabras que obtengamos del readWords()
+  const [word, setWord] = useState(); // para guardar la palabra que el usuario escribe en el input
+  const [isLoading, setIsLoading] = useState(false); // para indicar al usuario que la info se está cargando
 
   async function fetchWords() {
+    // función que trae las palabras de la blockchain, usando la función readWords() de nuestro smart contract
     if (typeof window.ethereum !== "undefined") {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(
@@ -20,21 +22,27 @@ function App() {
         provider
       );
       try {
+        setIsLoading(true);
         const data = await contract.readWords();
         setWords(data);
+        setIsLoading(false);
       } catch (err) {
         console.log("Error: ", err);
       }
+    } else {
+      alert("Tenés que tener Metamask instalada.");
     }
   }
 
   async function requestAccount() {
-    await window.ethereum.request({method: "eth_requestAccounts"});
+    // fuunción para obtener la cuenta de Metamask
+    await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
   async function newWord() {
+    // función para escribir una nueva palabra en la blockchain, usando la función createWord() de nuestro smart contract
     if (!word) return;
-    
+
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -51,24 +59,27 @@ function App() {
       fetchWords();
       setIsLoading(false);
     }
-
   }
 
   return (
     <div className="App">
+      <button onClick={fetchWords}>Traer las palabras</button>
+      <br />
+      <br />
+      <input
+        placeholder="Nueva palabra"
+        onChange={(e) => setWord(e.target.value)}
+      />
+      <button onClick={newWord}>Crear palabra</button>
+
+      <br />
+      <br />
+      {isLoading && <p>Cargando...</p>}
       <ul className="Words">
-        {words.map((word)=> (
+        {words.map((word) => (
           <li key={word}>{word}</li>
         ))}
       </ul>
-      
-      <button onClick={fetchWords}>Traer las palabras</button>
-      <br /><br />
-      <input placeholder="Nueva palabra" onChange={e => setWord(e.target.value)} />
-      <button onClick={newWord}>Crear palabra</button>
-
-      <br /><br />
-      {isLoading && <p>Cargando...</p>}
     </div>
   );
 }
